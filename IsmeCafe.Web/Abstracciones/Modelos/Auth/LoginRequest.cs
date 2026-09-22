060aad994b77bdf1e5f9687c7101f05bbb2b009a
Keylor Mora <kmora70001@ufide.ac.cr>
2026-09-21 21:03:11 -0600

    Iniciar sesión

--- IsmeCafe.Web/Abstracciones/Modelos/Auth/LoginRequest.cs
+++ IsmeCafe.Web/Abstracciones/Modelos/Auth/LoginRequest.cs
@@ -8,9 +8,12 @@ namespace Abstracciones.Modelos.Auth
         [EmailAddress(ErrorMessage = "Debe ingresar un formato de correo válido")]
         public string Correo { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "La contraseña es obligatoria")]
-        public string Contrasena { get; set; } = string.Empty;
+		[Required(ErrorMessage = "La contraseña es obligatoria")]
+		[RegularExpression(
+		@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$",
+		ErrorMessage = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un símbolo.")]
+		public string Contrasena { get; set; } = string.Empty;
 
-        public bool EsEmpleado { get; set; }
+		public bool EsEmpleado { get; set; }
     }
 }
